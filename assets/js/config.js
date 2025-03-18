08bf425ac443283cfc50592df4d4473197e8d4a4
Sai <82301356+imsaichauhan@users.noreply.github.com>
2025-03-19 04:56:01 +0530

    Update config.js
    VENUE_VIDEO

--- assets/js/config.js
+++ assets/js/config.js
@@ -8,6 +8,7 @@ const CONFIG = {
     EVENT: {
         DATE_TIME: 'March 25, 2025 at 12:00 PM',
         VENUE: "Nice try, Sherlock, but you’ll have to wait!",
+        VENUE_VIDEO: 'hello',
         GOOGLE_MAPS: 'Lost? Stay tuned, all will be revealed soon!',
         ITEMS: "A+ for effort, but no spoilers! Just bring your party energy when it’s time!",
     },
