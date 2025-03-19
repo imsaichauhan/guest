9efa3406e69ac8d50408b4499bb331cff6f23268
Sai <82301356+imsaichauhan@users.noreply.github.com>
2025-03-20 02:13:57 +0530

    Update config.js
    final

--- assets/js/config.js
+++ assets/js/config.js
@@ -6,14 +6,14 @@ const CONFIG = {
     WHATSAPP_LINK: 'https://chat.whatsapp.com/EGyQOOsqYllJZRNtNDTvQ9',
     // Event details
     EVENT: {
-        DATE_TIME: 'March 25, 2025 at 12:00 PM',
+        DATE_TIME: 'March 25, 2025 at 11:30 AM',
         VENUE: "Nice try, Sherlock, but you’ll have to wait!",
-        VENUE_VIDEO: 'https://www.youtube.com/',
+        VENUE_VIDEO: 'https://www.youtube.com/', 
         GOOGLE_MAPS: 'https://www.youtube.com/',
         ITEMS: "A+ for effort, but no spoilers! Just bring your party energy when it’s time!",
     },
     // Countdown target date (YYYY, MM-1, DD, HH, MM, SS)
-    TARGET_DATE: new Date(2025, 2, 25, 12, 0, 0), // March 25, 2025, 12:00 PM
+    TARGET_DATE: new Date(2025, 2, 25, 11, 30, 0), // March 25, 2025, 12:00 PM
     // New flag to control venue details reveal manually from backend
     REVEAL_VENUE: false // Set to true from backend when ready to unblur
 };
